ca1d7c8a28668c44d5ddff51b02025b0a4f6e4bb
Tonybobo <bochuangjie@gmail.com>
2022-11-27 15:37:24 +0800

    add user entity

---
+++ pkg/common/constant/constant.go
@@ -0,0 +1,21 @@
+package constant
+
+const (
+	HEART_BEAT = "heartbeat"
+	PONG       = "pong"
+
+	MESSAGE_TYPE_USER  = 1
+	MESSAGE_TYPE_GROUP = 2
+
+	TEXT       = 1
+	FILE       = 2
+	IMAGE      = 3
+	AUDIO      = 4
+	VIDEO      = 5
+	VOICE_CALL = 6
+	VIDEO_CALL = 7
+
+	// Channel Type with go channel or kafka
+	GO_CHANNEL = "gochannel"
+	KAKFA      = "kafka"
+)
